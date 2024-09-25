f10410502ff3e2ffb4b28411fd31356d9fede4ad
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2024-09-25 11:22:10 +0530

    fix#6

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
 import { authOptions } from "@/app/lib/auth";
 import NextAuth from "next-auth/next";
 
-const handler = NextAuth(authOptions);
+const authOptionsWithNoCache = {
+    ...authOptions,
+    cache: false,
+  }
+
+const handler = NextAuth(authOptionsWithNoCache);
 export { handler as GET, handler as POST };
